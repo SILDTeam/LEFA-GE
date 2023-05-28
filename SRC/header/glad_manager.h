53de5d89c35ade34b59826029d7c96ae80335003
Gustavo Simões <72417648+phikill@users.noreply.github.com>
2023-05-28 16:42:33 -0300

    ...

---
+++ SRC/header/glad_manager.h
@@ -0,0 +1,9 @@
+
+
+void consoleEngineGLStatus()
+{
+    printf("engine OpenGl Version : 2.1 \n");
+    const char* version = (const char*)glGetString(GL_VERSION);
+    printf("device OpenGL Version: %s\n", version);
+}
+

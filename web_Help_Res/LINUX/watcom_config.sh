d15445696bdd6494308066abcbaf491323025ce5
Gustavo Simões <72417648+phikill@users.noreply.github.com>
2023-07-28 16:20:16 -0300

    Add files via upload

---
+++ web_Help_Res/LINUX/watcom_config.sh
@@ -0,0 +1,8 @@
+# Linux 32-bit shell script:
+# ---------------------------------------------------------------------------
+export WATCOM=/usr/bin/WATCOM
+export PATH=$WATCOM/binl:$PATH
+export EDPATH=$WATCOM/eddat
+export INCLUDE=$WATCOM/lh
+export LIB=
+# ---------------------------------------------------------------------------

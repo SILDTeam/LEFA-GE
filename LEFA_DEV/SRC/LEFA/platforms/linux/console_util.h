bb614600ae0c3a54b6d318041ab8af8eebf02796
Gustavo Simões <72417648+phikill@users.noreply.github.com>
2023-08-01 20:29:53 -0300

    Update console_util.h

--- LEFA_DEV/SRC/LEFA/platforms/linux/console_util.h
+++ LEFA_DEV/SRC/LEFA/platforms/linux/console_util.h
@@ -1,17 +1,28 @@
-
-#include <unistd.h>
+/*Open LEFA Project
+2022-2023
+*/
+#include<stdio.h>
+#include<unistd.h>;
 
 //SHOW LINUX CONSOLE 
 
 void EnableHUDConsole() {
 }
 
+void ShowConsoleWindow() {
+    if (isatty(fileno(stdout))) {
+        printf("\033[2J"); // Limpar a tela do terminal usando ANSI escape sequence
+        fflush(stdout); // Certificar-se de que a saída seja exibida imediatamente
+    }
+}
 
+/*
 void ShowConsoleWindow() {
     if (isatty(fileno(stdout))) {
         system("clear");
     }
 }
+*/
 
 void HideConsoleWindow() {
     // Não faz nada, já que não há uma janela de console a ser ocultada no Linux

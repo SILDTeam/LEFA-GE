30071c2cccf2d45195e8264e3e82b7d381ed9296
phikill <141458656+PHIKILLA2@users.noreply.github.com>
2023-08-09 10:33:27 -0300

    Delete watcom_config.sh

--- web_Help_Res/LINUX/watcom_config.sh
+++
@@ -1,8 +0,0 @@
-# Linux 32-bit shell script:
-# ---------------------------------------------------------------------------
-export WATCOM=/usr/bin/WATCOM
-export PATH=$WATCOM/binl:$PATH
-export EDPATH=$WATCOM/eddat
-export INCLUDE=$WATCOM/lh
-export LIB=
-# ---------------------------------------------------------------------------
